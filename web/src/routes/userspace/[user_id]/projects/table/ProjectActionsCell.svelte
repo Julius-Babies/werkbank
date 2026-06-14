bfb53ea6ec48a1927236e7d79d0717a14ad08669
Julius Vincent Babies <julius@familie-babies.de>
2026-06-14 10:44:15 +0200

    open project url

--- web/src/routes/userspace/[user_id]/projects/table/ProjectActionsCell.svelte
+++ web/src/routes/userspace/[user_id]/projects/table/ProjectActionsCell.svelte
@@ -2,6 +2,9 @@
     import type {Project} from "../getProjects.ts";
     import {Button} from "$lib/components/ui/button";
     import {ArrowSquareOutIcon} from "phosphor-svelte";
+    import {get} from "svelte/store";
+    import {user} from "../../state.ts";
+    import {env} from "$env/dynamic/public";
 
     let {
         project,
@@ -12,6 +15,8 @@
 
 <div class="flex flex-row gap-2">
     <Button
+            href={"https://" + project.project_key + "." + get(user)!.username.toLowerCase() + "." + env.PUBLIC_BASE_DOMAIN}
+            target="_blank"
             size="icon-sm"
             variant="ghost"
     >
