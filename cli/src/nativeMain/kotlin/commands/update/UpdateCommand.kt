ffb350420b6d5f7588a7c43720a04fffe0555b4f
Julius Vincent Babies <julius@familie-babies.de>
2026-07-06 19:39:15 +0200

    #1 feat: Download and install latest update

--- cli/src/nativeMain/kotlin/commands/update/UpdateCommand.kt
+++ cli/src/nativeMain/kotlin/commands/update/UpdateCommand.kt
@@ -4,10 +4,15 @@ import app.config.MainConfig
 import app.werkbank.BuildKonfig
 import app.werkbank.shared.cli.update.UpdateResponse
 import com.github.ajalt.clikt.command.SuspendingCliktCommand
+import es.jvbabi.kfile.File
 import http.httpClient
 import io.github.z4kn4fein.semver.Version
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.bodyAsChannel
+import io.ktor.http.isSuccess
+import io.ktor.utils.io.asByteWriteChannel
+import io.ktor.utils.io.copyTo
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -24,8 +29,27 @@ class UpdateCommand: SuspendingCliktCommand("update"), KoinComponent {
             .body<UpdateResponse>()
 
         when (updateState) {
-            UpdateResponse.NoUpdate -> println("You are already on the latest version")
-            is UpdateResponse.UpdateAvailable -> println("Update available: ${updateState.version}")
+            UpdateResponse.NoUpdate -> {
+                println("You are already on the latest version")
+                return
+            }
+            is UpdateResponse.UpdateAvailable -> println("Update available: ${updateState.version} at ${updateState.downloadUrl}")
         }
+
+        val targetFile = File.getTempDirectory().resolve("wbcliupdatefile")
+        val downloadResponse = httpClient().get(updateState.downloadUrl)
+        if (!downloadResponse.status.isSuccess()) {
+            println("Failed to download update file: ${downloadResponse.status}")
+            return
+        }
+
+        println("Downloading update file...")
+        downloadResponse.bodyAsChannel().copyTo(targetFile.sink().asByteWriteChannel())
+
+        println("Update downloaded")
+        println("Installing update...")
+        targetFile.setExecutable(true)
+        targetFile.copy(File.getCurrentExecutableFile())
+        println("Update installed")
     }
 }
