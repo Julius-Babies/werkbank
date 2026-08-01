29deebf7b5ae8eab15f9afb26679da1e9871f084
Julius Vincent Babies <julius@familie-babies.de>
2026-08-01 10:36:28 +0200

    #64 Style error pages

--- web/src/routes/proxy/error/+layout.svelte
+++ web/src/routes/proxy/error/+layout.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-    import './layout.css';
     import faviconRed from '$lib/assets/logo_red.svg';
     import "$lib/localization/i18n";
-
 
     let {children} = $props();
 
@@ -12,6 +10,19 @@
     <link rel="icon" href={faviconRed}/>
 </svelte:head>
 
-<div class="flex w-full h-full">
-    {@render children()}
+<div class="flex flex-col w-full h-full">
+    <div class="flex flex-col grow">
+        {@render children()}
+    </div>
+
+    <div class="flex flex-col">
+        <hr />
+
+        <div class="flex flex-row items-center gap-2 px-4 py-2">
+            <img src={faviconRed} alt="logo" class="w-6 h-6"/>
+            <span class="text-sm font-mono text-accent-foreground">
+                Werkbank Cloud &bullet; This response does not originate from the target service
+            </span>
+        </div>
+    </div>
 </div>
